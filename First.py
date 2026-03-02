8b53b1c579a79a6d5598714673bb9d0fb092df77
realhappycrafter <13750905012@163.com>
2026-03-02 20:46:05 +0800

    Add files via upload

---
+++ First.py
@@ -0,0 +1,11 @@
+# print("Hello, World!")
+# input("Press Enter to exit...")
+# # This is a simple Python script that prints "Hello, World!" to the console and waits for the user to press Enter before exiting.
+# """ diyihangzhushi
+#  dierhangzhushi
+#   disanhangzhushi """
+
+# password = int(input("请输入密码："))
+# print("密码是：", password, "类型是：", type(password))
+import math
+print(math.pi**(math.pi**math.pi))
